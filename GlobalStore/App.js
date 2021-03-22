2fbeca46eca2d23f30c89f9d521b586117eb27ea
Yudhiesh Ravindranath <yudhiesh1997@gmail.com>
2021-03-22 14:30:10 +0800

    DONE

--- GlobalStore/App.js
+++ GlobalStore/App.js
@@ -1,11 +1,10 @@
-import {TodosProvider} from "./TodoStore"
+import { TodosProvider } from "./TodoStore";
 import Todos from "./Todos";
-
 
 function App() {
   return (
     <TodosProvider>
       <Todos />
-      <TodosProvider>
+    </TodosProvider>
   );
 }
