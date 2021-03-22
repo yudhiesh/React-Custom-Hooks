6472f0945d995f729a428849dd228ef36da26947
Yudhiesh Ravindranath <yudhiesh1997@gmail.com>
2021-03-22 13:45:54 +0800

    Global Store + localStorage

--- GlobalStore/makeStore.js
+++ GlobalStore/makeStore.js
@@ -1,8 +1,18 @@
 import React from "react";
 
-export default function makeStore(reducer, initialState) {
+export default function makeStore(userReducer, initialState) {
   const storeContext = React.createContext();
   const dispatchContext = React.createContext();
+
+  try {
+    initialState = JSON.parse(localStorage.getItem(key)) || initialState;
+  } catch {}
+
+  const reducer = (state, action) => {
+    const newState = userReducer(state, action);
+    localStorage.setItem(key, JSON.stringify(newState));
+    return newState;
+  };
 
   const StoreProvider = ({ children }) => {
     const [store, dispatch] = React.useReducer(reducer, initialState);
